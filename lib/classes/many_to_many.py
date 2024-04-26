0631f1ec4f01d529d26c352110a87693c137c6dc
Deborah Kurz <DeborahKurz6@gmail.com>
2024-04-26 17:01:08 -0600

    Complete Lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,7 +1,14 @@
+import sqlite3
+CONN = sqlite3.connect("national_parks.db")
+CURSOR = CONN.cursor()
+
+
 class NationalPark:
+    all = []
 
     def __init__(self, name):
         self.name = name
+        NationalPark.all.append(self)
 
     @property
     def name(self):
@@ -9,24 +16,44 @@ class NationalPark:
 
     @name.setter
     def name(self, name):
-        if  hasattr(self, '_name'):
-            raise AttributeError("Cannot change the name after instantiation.")
-        elif isinstance(name, str) and len(name) >= 3:
+        if  hasattr(self, 'name'):
+            raise Exception("Cannot change the name after instantiation.")
+        elif not isinstance(name, str) or not (len(name) >= 3):
+            raise Exception("Needs to be longer than 3")
+        else:
             self._name = name
-        return name
-        
 
     def trips(self):
-        pass
-    
+        return [trip for trip in Trip.all if type(Trip) and trip.national_park == self]
+
     def visitors(self):
-        pass
+        return list(set(trip.visitor for trip in Trip.all if type(Visitor) and trip.national_park == self))
     
     def total_visits(self):
-        pass
+        trip_list = self.trips()
+        if  len(trip_list) >= 1:
+            return len(trip_list)
+        else:
+            return 0
     
     def best_visitor(self):
-        pass
+        visitor_count = {}
+        for trip in Trip.all:
+            if trip.national_park == self:
+                visitor_count[trip.visitor] = visitor_count.get(trip.visitor, 0) + 1
+
+        if visitor_count:
+            max_visits = max(visitor_count.values())
+            best_visitors = [visitor for visitor, visits in visitor_count.items() if visits == max_visits]
+            return best_visitors[0]
+        else:
+            return None
+        # park_visitor = [trip.visitor for trip in Trip.all if trip.national_park == self]
+        # breakpoint()
+        # return [visitor for visitor in Visitor.all if visitor.name == park_visitor]
+        #search Trip for visitor with the most visits
+        #search Visitor for the visitor
+            #
 
 class Trip:
     all = []
@@ -59,11 +86,37 @@ class Trip:
             if format(end_date):
                 self._end_date = end_date
         return end_date
+    
+    @property
+    def visitor(self):
+        return self._visitor
+    
+    @visitor.setter
+    def visitor(self, visitor):
+        if isinstance(visitor, Visitor):
+            self._visitor = visitor
+        else:
+            raise ValueError("Invalid visitor type")
+        
+    @property
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park(self, national_park):
+        if isinstance(national_park, NationalPark):
+            self._national_park = national_park
+        else:
+            raise ValueError("Invalid national park type")
+
 
 class Visitor:
+    all = []
 
-    def __init__(self, name):
+    def __init__(self, name, id = None):
         self.name = name
+        self._id = id
+        Visitor.all.append(self)
 
     @property
     def name(self):
@@ -74,12 +127,12 @@ class Visitor:
         if isinstance(name, str) and 0 < len(name) < 16:
             self._name = name
         return name
-    
+
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if isinstance(trip, Trip) and trip.visitor == self]
     
     def national_parks(self):
-        pass
+        return list(set(trip.national_park for trip in Trip.all if trip.visitor == self and isinstance(trip.national_park, NationalPark)))
     
     def total_visits_at_park(self, park):
         pass
